a42edcf63ea8caf2fd2778c913a66e83b55cbfec
Nishant Sapkota <snishant306@gmail.com>
2021-09-07 16:23:40 +0545

    Hotfixes

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import os
 import socket
 import threading
+import typing as t
 
 import cv2
 import qrcode
 
 
 class Sharefile:
-    def __init__(self, file_path: str, port: int = 8000):
+    def __init__(self, file_path: str, port: t.Optional[int] = 8000):
         self.file_path = file_path
         self.port = port
 
     def create_qr_code(self):
         self.ip = socket.gethostbyname(socket.gethostname())
         print("Generating Link...")
-        if self.port == "":
-            self.port = 8000
         hosted_link = f"http://{self.ip}:{self.port}"
         print(hosted_link)
         print("Generating QR Code...")
@@ -36,21 +35,20 @@ class Sharefile:
         cv2.imshow("Scan Qr code and Open Link", img_read)
         cv2.waitKey(20000)
         cv2.destroyAllWindows()
-        os.system("del qrcode.png")
+        os.remove("qrcode.png")
 
 
-# _-------------------------------------------------------
+#------------------------------------------------------
 def run():
-    filepath = input("Input File or Folder Path To be Shared ~# ")
-    port = input("Input Port Number Enter(Default:8000) ~# ")
+    filepath = input("Input File or Folder Path To be Shared ~#")
+    port = input("Input Port Number Enter(Default:8000) ~#")
     object_ = Sharefile(file_path=filepath, port=port)
     object_.create_qr_code()
     t1 = threading.Thread(target=object_.show_qr)
     t2 = threading.Thread(target=object_.run_server)
     t1.start()
     t2.start()
-    t2.join()
-    t1.join()
 
 
-run()
+while __name__ == "__main__":
+    run()
